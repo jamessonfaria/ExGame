c4222e5a3dcdbd415525deafe3d87dcbc8473e3d
thiagoads0 <thiagoads@gmail.com>
2015-10-16 09:24:16 -0300

    Consultando Jogos do WS
    Consulta de Jogos e pequenos ajustes

---
+++ Jogo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExGame
+{
+    [DataContract]
+    class Jogo
+    {
+        [DataMember(Name = "descricao")]
+        public string Descricao { get; set; }
+
+        [DataMember(Name = "console")]
+        public string Console { get; set; }
+
+        //url da imagem da foto
+        [DataMember(Name = "foto")]
+        public string Foto { get; set; }
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -8,15 +8,22 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using ExGame.Resources;
+using System.Xml.Linq;
+using System.Runtime.Serialization.Json;
 
 namespace ExGame
 {
     public partial class MainPage : PhoneApplicationPage
     {
+
+        ProgressIndicator progress;
+
         // Constructor
         public MainPage()
         {
+            progress = new ProgressIndicator();
             InitializeComponent();
+            BaixarJogos();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -25,7 +32,7 @@ namespace ExGame
             //Verificar se é o primeiro acesso do usuário através de consulta ao PhoneSettings
             if (false)
             {
-                Configurar_Perfil();
+                ConfigurarPerfil();
             }
         }
 
@@ -42,9 +49,43 @@ namespace ExGame
             NavigationService.Navigate(destino);
         }
 
-        private void Configurar_Perfil() {
+        private void ApplicationBarIconButton_Configuracoes_Click(object sender, EventArgs e)
+        {
+            Uri destino = new Uri("/ConfiguacoesPage.xaml", UriKind.Relative);
+            NavigationService.Navigate(destino);
+        }
+
+        private void ConfigurarPerfil() {
             Uri destino = new Uri("/PerfilPage.xaml", UriKind.Relative);
             NavigationService.Navigate(destino);
+        }
+
+        
+
+        private void BaixarJogos()
+        {
+            progress.IsVisible = true;
+            progress.IsIndeterminate = true;
+            progress.Text = "Baixando Jogos...";
+
+            SystemTray.SetProgressIndicator(this, progress);
+
+            string url = "https://srvwebservice.herokuapp.com/api/v1/jogos";
+
+            WebClient client = new WebClient();
+            client.OpenReadCompleted += Client_OpenReadCompleted;
+            client.OpenReadAsync(new Uri(url, UriKind.Absolute));
+
+        }
+
+        private void Client_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(List<Jogo>));
+            List<Jogo> jogos = (List<Jogo>)serializer.ReadObject(e.Result);
+            lbJogos.ItemsSource = jogos;
+            progress.IsVisible = false;
+
         }
 
     }
