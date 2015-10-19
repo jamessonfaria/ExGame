604819ba6978591c466a962566d4f5f02bb93554
thiagoads0 <thiagoads@gmail.com>
2015-10-19 00:06:48 -0200

    PerfilJogosPage

---
+++ JogoPerfil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExGame
+{
+    [DataContract]
+    class JogoPerfil
+    {
+        [DataMember(Name = "jogo_id")]
+        public int JogoId { get; set; }
+
+        [DataMember(Name = "perfil_id")]
+        public int PerfilId { get; set; }
+    }
+}
